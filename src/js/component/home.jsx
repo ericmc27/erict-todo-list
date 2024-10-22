70da4c79241738a0885a24fa6c42b0227ac90e55
Eric Castillo <erictomas27@hotmail.com>
2024-10-22 22:19:43 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,48 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useEffect, useState, useRef} from "react";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([{"text": ""}])
+	const todosRefs = useRef([]);
+	const dot = <div className="dot"></div>
+	
+	const handleInput = (index, event)=>{
+			const newTodos = [...todos];
+			newTodos[index].text = event.target.value;
+			setTodos(newTodos);
+
+	}
+
+	const addTodo = (event)=>{
+		const newTodos = [...todos, {"text":""}]
+		setTodos(newTodos)
+	}
+
+	useEffect(() => {
+		if (todosRefs.current[todos.length - 1]) {
+			todosRefs.current[todos.length - 1].focus(); // Focus on the new input
+		}
+	}, [todos]); // Run this effect when todos change
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			{todos.map((todo, index)=>(
+				<div className="page" style={{marginTop: index===0 ? "25px" : "0px"}}>
+					{dot}
+					<input
+					ref={(el)=>(todosRefs.current[index] = el)}
+					type="text"
+					value={todo.text}
+					onChange={(event)=>{handleInput(index, event)}}
+					onKeyUp={(e)=>{if(e.key === "Enter"){addTodo(e)}}}
+					disabled={index < todos.length - 1}/>
+							
+				</div>
+			))}
+		
+			<div className="bottom-block">{todos.length-1} {todos.length===0 ? "item" : "items"} left</div>
+		
+		</>
 	);
 };
 
