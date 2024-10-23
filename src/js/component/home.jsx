652449110b5947bf9dfa4bdd136d56f9192d6d43
Eric Castillo <erictomas27@hotmail.com>
2024-10-23 16:23:41 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,39 @@
 import React, { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [todos, setTodos] = useState([{ "text": "" }])
+	const [todos, setTodos] = useState([{ id: 1, "text": "" }]) 
+	const [lastTodoIndex, setLastTodoIndex] = useState(-1);
 	const currentRef = useRef([]);
-
-	const handleText = (event, index)=>{
+	
+	console.log(todos)
+	const handleTextChange = (event, index)=>{
 		const newTodo = [...todos]
 		newTodo[index].text = event.target.value;
 		setTodos(newTodo);
 	}
 
-	const addNewTodo = ()=>{
-		const newTodo = [...todos, {"text":""}]
+	const addNewTodo = (index)=>{
+		const newTodo = [...todos, {id: todos[index].id+1, "text":""}]
 		setTodos(newTodo);
+		setLastTodoIndex(todos.length)
+	}
+
+	const removeTodo = (id)=>{
+		const newTodo = todos.filter(todo => todo.id !== id);
+
+		if(todos.length===1){
+			setTodos([{id: 1, "text":""}])
+		}else{
+			setTodos(newTodo);
+		}
+
+		setLastTodoIndex(todos.length-1)
 	}
 
 	useEffect(()=>{
@@ -29,20 +46,21 @@ const Home = () => {
 		<>
 
 			{todos.map((todo, index) => (
-				<>
-					<div className="page">
+					<div className="page" style={{marginTop: todos.indexOf(todo)===0 ? "25px" : "0px"}}>
 						<div className="dot"></div>
 						<input
 							type="text"
 							value={todo.text}
+							placeholder={todos.length===1?"No tasks, add a task." : ""}
 							ref={(e)=>(currentRef.current[index] = e)}
-							onChange={(event)=>{handleText(event, index)}}
-							onKeyUp={(event)=>{if(event.key==="Enter"){addNewTodo()}}}
+							onChange={(event)=>{handleTextChange(event, index)}}
+							onKeyUp={(event)=>{if(event.key==="Enter"){addNewTodo(index)}}}
+							disabled={index < lastTodoIndex}
 						/>
+						{index >= 0 && <FontAwesomeIcon className="icon" onClick={()=>{removeTodo(todo.id)}} icon={faCircleXmark} size="lg" style={{color: "#e62f0f", marginTop: "18px", marginRight: "10px"}} />}
 					</div>
-				</>
 			))}
-			<div className="bottom-block"></div>
+			<div className="bottom-block">{todos.length-1}</div>
 		</>
 	);
 };
